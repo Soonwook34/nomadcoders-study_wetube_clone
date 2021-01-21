f102147497a31db5751499f0ac1f9d44233a787a
SOONWOOK PARK <soonwook34@gmail.com>
2021-01-21 17:39:23 +0900

    #2.4 Handling Routes with Express

--- wetube/index.js
+++ wetube/index.js
@@ -4,9 +4,24 @@ const app = express();
 // 포트 번호 지정
 const PORT = 4000;
 
+// respond 함수
 function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
-// 서버 시작
+function handleHome(req, res) {
+    console.log(req);
+    res.send("Hello from Home");
+}
+
+function handleProfile(req, res) {
+    res.send("You are on my profile");
+}
+
+// route 생성
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+// server 시작
 app.listen(PORT, handleListening);
