32fa8483d7517cd32eb9667c5a501d85e527d8fc
SOONWOOK PARK <soonwook34@gmail.com>
2021-01-21 18:58:14 +0900

    #2.6 Express Core: Middlewares

--- wetube/index.js
+++ wetube/index.js
@@ -8,12 +8,18 @@ const PORT = 4000;
 // respond 함수 (using Arrow Function)
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => {
-    console.log(req);
-    res.send("Hello from Home");
-}
+const handleHome = (req, res) => res.send("Hello from Home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+//middleware 함수
+const betweenHome = (req, res, next) => {
+    console.log("I'm between");
+    next();
+}
+
+// middleware global 사용
+app.use(betweenHome);
 
 // route 생성
 app.get("/", handleHome);
