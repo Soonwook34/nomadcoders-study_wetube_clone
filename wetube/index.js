a0c5c7269e53f6186e7d4bccff540f6df02066ab
SOONWOOK PARK <soonwook34@gmail.com>
2021-01-21 19:07:36 +0900

    #2.7 Express Core: Middlewares part Two

--- wetube/index.js
+++ wetube/index.js
@@ -1,5 +1,9 @@
 import express from "express";
-// const express = require('express');
+// const express = require('express'); => babel 사용으로 대체
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 const app = express();
 
 // 포트 번호 지정
@@ -20,10 +24,17 @@ const betweenHome = (req, res, next) => {
 
 // middleware global 사용
 app.use(betweenHome);
+// middleware bodyParser, cookieParser(json, urlencoded) 사용
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// helmet middleware 사용 for security
+app.use(helmet());
+// morgan middleware 사용 for logging (combined / common / dev / short / tiny)
+app.use(morgan("dev"));
 
 // route 생성
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
 
 // server 시작
