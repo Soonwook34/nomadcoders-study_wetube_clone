6c28d9aa81d5719e05796ba988d384d3a7359f9b
SOONWOOK PARK <soonwook34@gmail.com>
2021-01-21 18:51:01 +0900

    #2.5 ES6 on NodeJS using Babel

--- wetube/index.js
+++ wetube/index.js
@@ -1,22 +1,19 @@
-const express = require('express');
+import express from "express";
+// const express = require('express');
 const app = express();
 
 // 포트 번호 지정
 const PORT = 4000;
 
-// respond 함수
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+// respond 함수 (using Arrow Function)
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome(req, res) {
+const handleHome = (req, res) => {
     console.log(req);
     res.send("Hello from Home");
 }
 
-function handleProfile(req, res) {
-    res.send("You are on my profile");
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 // route 생성
 app.get("/", handleHome);
