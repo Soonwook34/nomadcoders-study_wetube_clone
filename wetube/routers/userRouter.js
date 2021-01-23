68ba41aadf15c73e65af3e0702f9613138c5b763
SOONWOOK PARK <soonwook34@gmail.com>
2021-01-23 12:08:28 +0900

    #2.11 MVC Pattern part Three

--- wetube/routers/userRouter.js
+++ wetube/routers/userRouter.js
@@ -3,9 +3,5 @@ import express from "express";
 // user router 생성
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res) => res.send("user index"));
-userRouter.get("/edit", (req, res) => res.send("user edit"));
-userRouter.get("/password", (req, res) => res.send("user password"));
-
 // userRouter export
 export default userRouter;
