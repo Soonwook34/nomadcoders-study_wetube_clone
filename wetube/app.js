85bb48cc8f40b13c211a63cc355d81838c9dfa02
SOONWOOK PARK <soonwook34@gmail.com>
2021-01-25 13:12:03 +0900

    #2.16 Local Variables in Pug

--- wetube/app.js
+++ wetube/app.js
@@ -4,33 +4,28 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes"
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes"
 
 const app = express();
 
-//middleware 함수
-const betweenHome = (req, res, next) => {
-    console.log("I'm between");
-    next();
-}
+// helmet middleware 사용 for security
+app.use(helmet());
 
 // View Engine을 Pug로 설정
 app.set("view engine", "pug");
 
-// middleware global 사용
-app.use(betweenHome);
 // middleware bodyParser, cookieParser(json, urlencoded) 사용
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// helmet middleware 사용 for security
-app.use(helmet());
 // morgan middleware 사용 for logging (combined / common / dev / short / tiny)
 app.use(morgan("dev"));
-
+// middleware 
+app.use(localsMiddleware);
 
 // routing 설정
 app.use(routes.home, globalRouter);
