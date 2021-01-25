9a8ad3630549087bf78ccc935546472bfa220ad3
SOONWOOK PARK <soonwook34@gmail.com>
2021-01-25 11:57:50 +0900

    #2.14 Layouts with Pug

--- wetube/routers/userRouter.js
+++ wetube/routers/userRouter.js
@@ -10,7 +10,7 @@ import {
 // user router 생성
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
+// userRouter.get(routes.users, users);
 userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
