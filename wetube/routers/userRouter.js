82cf5a850c81175689d7ac9ffb7b922a08d7734f
SOONWOOK PARK <soonwook34@gmail.com>
2021-01-25 11:32:40 +0900

    #2.12 Recap

--- wetube/routers/userRouter.js
+++ wetube/routers/userRouter.js
@@ -1,7 +1,19 @@
 import express from "express";
+import routes from "../routes";
+import {
+    users,
+    userDetail,
+    editProfile,
+    changePassword
+} from "../controllers/userController";
 
 // user router 생성
 const userRouter = express.Router();
+
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 // userRouter export
 export default userRouter;
