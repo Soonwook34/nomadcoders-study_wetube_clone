918b5400c5f15f7b1d8538dbb125d0784ebba145
SOONWOOK PARK <soonwook34@gmail.com>
2021-01-28 19:56:39 +0900

    #2.25 More Controlers

--- wetube/controllers/userController.js
+++ wetube/controllers/userController.js
@@ -6,16 +6,14 @@ export const getJoin = (req, res) => {
   
 export const postJoin = (req, res) => {
     // req.body => bodyParser using ECMAScript6(ES6)
-    const {
-      body: { name, email, password, password2 }
-    } = req;
+    const { body: { name, email, password, password2 } } = req;
     if (password !== password2) {
-      res.status(400);
-      res.render("join", { pageTitle: "Join" });
+        res.status(400);
+        res.render("join", { pageTitle: "Join" });
     } else {
-      // To Do: Register User
-      // To Do: Log user in
-      res.redirect(routes.home);
+        // To Do: Register User
+        // To Do: Log user in
+        res.redirect(routes.home);
     }
 };
   
@@ -24,7 +22,12 @@ export const postLogin = (req, res) => {
     res.redirect(routes.home);
 };
 
-export const logout = (req, res) => res.render("logout", { pageTitle: "Log Out" });
+export const logout = (req, res) => {
+    // To Do: Process Log Out
+    res.redirect(routes.home);
+};
+
+
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
