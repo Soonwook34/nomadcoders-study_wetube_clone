8c79eaf18c5d0c1548ed8543bb726fd08531f3eb
SOONWOOK PARK <soonwook34@gmail.com>
2021-02-01 12:36:13 +0900

    #3.3 Video Model

--- wetube/init.js
+++ wetube/init.js
@@ -1,8 +1,11 @@
 import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
 
-// 포트 번호 지정
-const PORT = 4000;
+// dotenv 사용 포트 번호 지정
+const PORT = process.env.PORT || 4000;
 
 // respond 함수 (using Arrow Function)
 const handleListening = () => console.log(`✅ Listening on: http://localhost:${PORT}`);
