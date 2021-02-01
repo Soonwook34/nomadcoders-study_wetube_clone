6557883b28c9a931d920e09e3e0cfd474e77de64
SOONWOOK PARK <soonwook34@gmail.com>
2021-02-01 11:50:15 +0900

    #3.1 Connecting to MongoDB

--- wetube/db.js
+++ wetube/db.js
@@ -1,50 +1,17 @@
-export const videos = [
+import mongoose from "mongoose";
+
+mongoose.connect(
+    "mongodb://localhost:27017/we-tube",
     {
-      id: 111111,
-      title: "Video awesome",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 1000001,
-        name: "Soonwook",
-        email: "soon@wook.com"
-      }
-    },
-    {
-      id: 222222,
-      title: "Video super",
-      description: "This is something I love",
-      views: 254,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 1000001,
-        name: "Soonwook",
-        email: "soon@wook.com"
-      }
-    },
-    {
-      id: 333333,
-      title: "Video nice",
-      description: "This is something I love",
-      views: 35,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 1000001,
-        name: "Soonwook",
-        email: "soon@wook.com"
-      }
-    },
-    {
-      id: 444444,
-      title: "Video perfect",
-      description: "This is something I love",
-      views: 3465,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 1000001,
-        name: "Soonwook",
-        email: "soon@wook.com"
-      }
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-  ];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
--- wetube/init.js
+++ wetube/init.js
@@ -1,10 +1,11 @@
+import "./db";
 import app from "./app";
 
 // 포트 번호 지정
 const PORT = 4000;
 
 // respond 함수 (using Arrow Function)
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`✅ Listening on: http://localhost:${PORT}`);
 
 // server 시작
 app.listen(PORT, handleListening);
