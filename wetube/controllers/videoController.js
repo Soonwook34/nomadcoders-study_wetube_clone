fffe189e6cd36e6550d8be00fdcb0ff39ad01783
SOONWOOK PARK <soonwook34@gmail.com>
2021-02-03 18:21:41 +0900

    #3.9 Editing a Video

--- wetube/controllers/videoController.js
+++ wetube/controllers/videoController.js
@@ -16,6 +16,7 @@ export const search = (req, res) => {
     const { query: { term: searchingBy } } = req;
     res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
+
 export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" });
 export const postUpload = async (req, res) => {
     const {
@@ -42,5 +43,29 @@ export const videoDetail = async (req, res) => {
     }
 };
 
-export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "editVideo" });
+export const getEditVideo = async (req, res) => {
+    const {
+        params: { id }
+    } = req;
+    try {
+        const video = await Video.findById(id);
+        res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+    } catch (error) {
+        res.redirect(routes.home);
+    }
+};
+  
+export const postEditVideo = async (req, res) => {
+    const {
+        params: { id },
+        body: { title, description }
+    } = req;
+    try {
+        await Video.findOneAndUpdate({ _id: id }, { title, description });
+        res.redirect(routes.videoDetail(id));
+    } catch (error) {
+        res.redirect(routes.home);
+    }
+};
+
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
