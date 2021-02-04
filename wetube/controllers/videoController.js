420b738becf2dfcb0256dcf7e81a484bf25ddd02
SOONWOOK PARK <soonwook34@gmail.com>
2021-02-04 12:48:24 +0900

    3.12 오타 수정

--- wetube/controllers/videoController.js
+++ wetube/controllers/videoController.js
@@ -4,7 +4,7 @@ import Video from "../models/Video";
 // Home
 export const home = async (req, res) => {
     try {
-        const videos = await Video.find({}).sort({ _id: -1 });;
+        const videos = await Video.find({}).sort({ _id: -1 });
         res.render("home", { pageTitle: "Home", videos });
     } catch (error) {
         console.log(error);
