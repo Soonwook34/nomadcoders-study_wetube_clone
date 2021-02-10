44295c5db5f28548e812facdf62db5733c306ba4
SOONWOOK PARK <soonwook34@gmail.com>
2021-02-10 22:08:23 +0900

    #6.1 Local Authentication with Passport part One

--- wetube/init.js
+++ wetube/init.js
@@ -1,8 +1,12 @@
+import dotenv from "dotenv";
 import "./db";
 import app from "./app";
-import dotenv from "dotenv";
+
 dotenv.config();
+
 import "./models/Video";
+import "./models/Comment";
+import "./models/User";
 
 // dotenv 사용 포트 번호 지정
 const PORT = process.env.PORT || 4000;
