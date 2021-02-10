6d72b3f80d01ee3dcd0e5969064af6fe5255872e
SOONWOOK PARK <soonwook34@gmail.com>
2021-02-10 22:18:09 +0900

    #6.2 Local Authentication with Passport part Two

--- wetube/controllers/userController.js
+++ wetube/controllers/userController.js
@@ -12,7 +12,7 @@ export const postJoin = async (req, res) => {
         res.status(400);
         res.render("join", { pageTitle: "Join" });
     } else {
-        // To Do: Register User
+        // Register User
         try {
             const user = await User({
                 name,
