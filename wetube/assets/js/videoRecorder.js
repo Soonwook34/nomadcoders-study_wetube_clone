a36f4c9475bdd1bd275af727acaa457dac0bf4be
SOONWOOK PARK <soonwook34@gmail.com>
2021-02-18 22:41:28 +0900

    #9.1 Recording Video part One

--- wetube/assets/js/videoRecorder.js
+++ wetube/assets/js/videoRecorder.js
@@ -2,7 +2,19 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+let streamObject;
+
+const handleVideoData = event => {
+    console.log(event);
+};
+
+const startRecording = () => {
+    const videoRecorder = new MediaRecorder(streamObject);
+    videoRecorder.start();
+    videoRecorder.addEventListener("dataavailable", handleVideoData);
+};
+
+const getVideo = async () => {
     try {
         // 권한 물어보기
         const stream = await navigator.mediaDevices.getUserMedia({
@@ -12,16 +24,21 @@ const startRecording = async () => {
         videoPreview.srcObject = stream;
         videoPreview.muted = true;
         videoPreview.play();
+        recordBtn.innerHTML = "Stop recording";
+        streamObject = stream;
+        startRecording();
     } catch (error) {
-        console.log(error);
         // 권한 없음 or 실패
+        console.log(error);
         recordBtn.innerHTML = "☹️ Cant record";
-        recordBtn.removeEventListener("click", startRecording);
+    } finally {
+        // 녹화 버튼을 한 번 누르면 비활성화
+        recordBtn.removeEventListener("click", getVideo);
     }
 };
 
 function init() {
-    recordBtn.addEventListener("click", startRecording);
+    recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
